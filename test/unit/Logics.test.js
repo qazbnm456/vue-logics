d9fc9f37b9c4c1ce62b4b8021ffecb6ef2e41030
Boik <qazbnm456@gmail.com>
2020-03-27 01:25:42 +0800

    chore: add more tests

--- test/unit/Logics.test.js
+++ test/unit/Logics.test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import Logics from '../../src/Logics.ts';
 
-test('can merge simple object', (t) => {
+test('Logics - Can merge simple object', (t) => {
   const a = {
     optionA: true,
     optionB: '1234',
@@ -12,6 +12,66 @@ test('can merge simple object', (t) => {
 
   expect(Logics.merge(a, b)).to.deep.equal({
     optionA: false,
-    optionB: '1234'
-  })
+    optionB: '1234',
+  });
+});
+
+test('Logics - Can add to simple array', (t) => {
+  const a = {
+    optionA: true,
+    optionB: ['first', 'second'],
+  };
+  const b = {
+    optionB: ['third'],
+  };
+
+  expect(Logics.merge(a, b, true)).to.deep.equal({
+    optionA: true,
+    optionB: ['first', 'second', 'third'],
+  });
+});
+
+test('Logics - Can merge recursively', (t) => {
+  const a = {
+    optionA: true,
+    optionC: {
+      first: '123',
+      third: {
+        a: 'b',
+      },
+    },
+    optionB: '1234',
+  };
+  const b = {
+    optionB: '567',
+    optionC: {
+      first: '1234',
+      second: '789',
+    },
+  };
+
+  expect(Logics.merge(a, b)).to.deep.equal({
+    optionA: true,
+    optionC: {
+      first: '1234',
+      third: {
+        a: 'b',
+      },
+      second: '789',
+    },
+    optionB: '567',
+  });
+});
+
+test('Logics - Can install on vue instance', (t) => {
+  const components = ['VueLogics'];
+  const registry = [];
+
+  function Vue () {};
+  Vue.component = function (name, instance) {
+    registry.push(name);
+  };
+
+  Logics.install(Vue, { extended: true });
+  expect(registry).to.deep.equal(components);
 });
