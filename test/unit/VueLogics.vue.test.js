85f431ecd729eda84e9bfc83267489f94e030bc3
Boik <qazbnm456@gmail.com>
2020-04-01 02:00:37 +0800

    chore: test

--- test/unit/VueLogics.vue.test.js
+++ test/unit/VueLogics.vue.test.js
@@ -4,6 +4,9 @@ import VueLogics from '../../src/VueLogics.vue';
 
 test('VueLogics.vue', (t) => {
   const wrapper = shallowMount(VueLogics);
-  expect(wrapper.findAll('#sidebar').length).to.equal(1);
-  expect(wrapper.findAll('#canvas').length).to.equal(1);
+  expect(wrapper.findAll('div.vue-logics').length).to.equal(1);
+  expect(wrapper.findAll('#logics-sidebar').length).to.equal(1);
+  expect(wrapper.findAll('#logics-container').length).to.equal(1);
+  expect(wrapper.findAll('#logics-canvas').length).to.equal(1);
+  expect(wrapper.findAll('#logics-temp-area').length).to.equal(1);
 });
